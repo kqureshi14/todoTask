36184f48430f6897274e00f9d9636c0dba8c8035
Kaleem <kaleem14you@gmail.com>
2024-08-12 20:25:20 +0500

    todoTask assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todoTask';
+
+  list:any[] = [];
+  addTask(item:any){
+    console.info(item);
+
+    this.list.push({id:this.list.length,name:item});
+  }
+
+  removeTask(id:number){
+    console.info(id);
+
+    this.list = this.list.filter((item)=>item.id!=id);
+
+  }
 }
